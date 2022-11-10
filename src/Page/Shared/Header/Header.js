09264918033135f8964bff57a4b1344c4dfe8dc6
mostafizurRahaman <fahim654326@gmail.com>
2022-11-10 17:48:23 +0600

    add some class on menu section

--- src/Page/Shared/Header/Header.js
+++ src/Page/Shared/Header/Header.js
@@ -14,7 +14,7 @@ const Header = () => {
             <img src={logo} alt="dentist" className="w-14 h-14 " />
             <h2 className="text-3xl font-bold uppercase">Mr. Dentist</h2>
          </Link>
-         <div className={`flex absolute flex-col  md:static md:flex-row items-center gap-5 font-medium text-xl  py-5 md:py-0 w-full h-screen md:w-auto top-20 transition-all md:transition-none duration-1000  bg-gray-800 md:bg-transparent md:bg-opacity-100 z-30 bg-opacity-80  ${display ? 'right-0' : 'goleft' }`}>
+         <div className={`flex absolute flex-col  md:static md:flex-row items-center gap-5 font-medium text-xl  py-5 md:py-0 w-full h-screen md:w-auto top-20 transition-all md:transition-none duration-1000  bg-gray-800 md:bg-transparent md:bg-opacity-100 z-30 bg-opacity-80  ${display ? 'left-0' : 'goleft' }`}>
             <Link to="/home" onClick={()=> setDisplay(!display)}>Home</Link>
             <Link to="/services" onClick={()=> setDisplay(!display)}>Services</Link>
             <Link to="/blog" onClick={()=> setDisplay(!display)}>Blog</Link>
